1060e56d686f86d037bb13669a385bdad0c434b6
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-16 20:53:05 -0300

    Re-fazendo com o for
    -Refiz o mesmo exercício, porém dessa vez usando o for, bem mais pratico.

--- aula14ex/ex018 correcao - outra opcao/assets/script.js
+++ aula14ex/ex018 correcao - outra opcao/assets/script.js
@@ -5,26 +5,27 @@ function clicou(){
     if(numentrada.value.length == 0){
         alert('Erro - Voce precisa digitar um numero!')
     }else{
-        // converteu o nun em numero só dentro do else.
+        
         let nun = Number(numentrada.value);
-        let c = 1; // contador começou no 1
-
-        // '' ou null da na mesma.
+       
         res.innerHTML = null; 
-        //limpa antes de começar 
-
-        while(c <= 10){
-            // criou dinamicamente um campo dentro do option
+        /**
+         * 
+         * Fazendo o mesmo excercicio, porém dessa vez com o for, parece mais simples.
+         * 
+         * 
+         * 
+         */
+        for(let c = 1; c <= 10;c++){
             let item = document.createElement('option')
-            // cada item vai receber o texto, com a seguinte infomração
+            
             item.text =`${nun} X ${c} = ${nun * c}`
-            // cada item vai ter esse valor. ** note que ele colocu a variavel e cada item novo vai receber um numero novo também.
+           
             item.value =`res${c}`
-            // adiciona itens abaixo de outro, no caso vai adicionar um item embaixo  de no caso o selection e vai herdar as infos.
+          
             res.appendChild(item)
-            c++
         }
-
+      
 
 
         
