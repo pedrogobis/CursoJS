2e16461972395dd504e1f8c0de9591bf77bfb059
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-16 20:07:28 -0300

    Algumas alterações

--- aula14ex/ex018/assets/script.js
+++ aula14ex/ex018/assets/script.js
@@ -7,12 +7,13 @@ function clicou(){
     // para substituir o atual colque um inner antes de executar a funçao
     
     res.innerHTML =`Tabuada do ${nun}`
-    if(nun == 0){
+    if(numentrada.value.length == 0){
         alert('Erro - Voce precisa digitar um numero!')
+        res.innerHTML ='<p class="error">Digite um numero valido, pó &#x1F937</p>'
     }else{
         while(contador <= 10){
             var resultado = contador * nun;
-            res.innerHTML +=`<p>${nun} X ${contador} = <strong>${resultado}</strong></p>`
+            res.innerHTML +=`<p class="result">${nun} X ${contador} = <strong>${resultado}</strong></p>`
             contador++;
         }
 
