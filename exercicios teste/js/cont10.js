63e7005c56fca1bdd45be8edff9e5751008eef99
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-16 09:56:08 -0300

    Update cont10.js

--- exercicios teste/js/cont10.js
+++ exercicios teste/js/cont10.js
@@ -4,9 +4,9 @@ function clicou(){
     var contador = 0;
     
 
-    res.innerHTML += `<p>Contando até 10</p>`;
+    res.innerHTML += `<p>Contando até 10</p>`;// pra pular linha tem que colocar tag e eu esqueci disso também
     while(contador<=10){
-        res.innerHTML += `${contador} &#x1F51C`
+        res.innerHTML += `${contador} &#x1F51C` // Meu erro estava aqui, eu esqueci de colocar o + na frente do = então ele só estava printando o ultimo numero
         contador++
     }
     res.innerHTML += `&#x1F602`
