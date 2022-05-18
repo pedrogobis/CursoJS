7d1490f2976d70643702aea0ecc760b2cb683b22
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-05-18 10:54:14 -0300

    update
    - Corrigi o maior e menor
    - Inseri a limpeza automatica do campo de input
    - Corrigido a formatação das respostas.

--- aula17ex/assets/analisa.js
+++ aula17ex/assets/analisa.js
@@ -1,9 +1,11 @@
 let res = document.querySelector('#res');
 let numero = document.querySelector('#inputnumero');
+let inputlimp = document.querySelector('#inputnumero');
 let numerosInseridos = [];
 //definindo a variavel do array como global, consegui corrigir o problema de substituição de numeros, agora está funcionando 
 
 function fazer(){
+    
     let num = Number(numero.value); // convertendo o valor digitado para numero, embora como definimos o imput como numero, imagino** que já converta automaticamente.
     numerosInseridos.push(num)
 
@@ -38,24 +40,33 @@ function fazer(){
         
     
         
-
+        inputlimp.value='';
     }
 
 
 function finalizar(){
     let finalizardados = document.querySelector('#resultadoFinalizar')
 
-    var max = Math.max(numerosInseridos);
-    var min = Math.min(numerosInseridos);
-    var soma = numerosInseridos.reduce(function(soma, i) {
+    let max = numerosInseridos.reduce(function(a, b) {
+        return Math.max(a, b);
+      }, -Infinity);
+
+    let min = numerosInseridos.reduce(function(a, b) {
+        return Math.min(a, b);
+      }, +Infinity);
+
+
+    console.log(min, max)
+
+    let soma = numerosInseridos.reduce(function(soma, i) {
         return soma + i;
     });
 
-    finalizardados.innerHTML = `Ao todo, temos ${numerosInseridos.length} cadastrados\n`
+    finalizardados.innerHTML = `<p>Ao todo, temos <strong>${numerosInseridos.length}</strong> numeros cadastrados</p>\n`
 
-    finalizardados.innerHTML += `O maior valor informado foi ${max}\n`
-    finalizardados.innerHTML +=`O menor informado foi ${min}`
-    finalizardados.innerHTML +=`Somando todos os valores ${soma} `
-    finalizardados.innerHTML +=`a media dos valores digitado é `
+    finalizardados.innerHTML += `<p>O maior valor informado foi <strong>${max}</strong></p>`
+    finalizardados.innerHTML +=`<p>O menor informado foi <strong>${min}</strong></p>`
+    finalizardados.innerHTML +=`<p>Somando todos os valores <strong>${soma}</strong></p> `
+    finalizardados.innerHTML +=`<p>A media dos valores digitado é <strong>${media}</strong></p> `
 }
 
